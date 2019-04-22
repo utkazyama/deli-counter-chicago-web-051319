116f37737dd1ebb525410097df1b13010b7b004b
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-22 18:44:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,5 +13,5 @@ end
 
 def take_a_number(array, customer)
   array << customer
-  puts "Welcome, #{customer}. You are number #{array.length+1} in line."
+  puts "Welcome, #{customer}. You are number #{array.length} in line."
 end
