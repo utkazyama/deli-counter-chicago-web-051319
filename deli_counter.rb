ad5639960e11dbc12a4e3e434011f03842fa0dcf
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-22 18:35:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   else
     current_line= "The line is currently:"
     array.each_with_index do |customer, index|
-      current_line << "#{index+1}. #{customer}"
+      current_line << " #{index+1}. #{customer}"
     end
     puts current_line
   end
