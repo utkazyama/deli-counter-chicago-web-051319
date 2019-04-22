61233a434bc1f1593ff357a88900610969e2d381
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-22 18:14:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 def line(array)
-  
+  current_line = []
   if array.length==0 
     puts "The line is currently empty."
   else
     
-    puts "The line is currently:"
+    puts "The line is currently: #{current_line}"
   end
 end
