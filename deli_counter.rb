d640a2f8efd2f4e7f3cd3b299ce6ba6d26276344
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-22 18:50:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def now_serving(array)
   if array.length == 0 
     puts "There is nobody waiting to be served!"
      else
-      puts "Currently serving #{array.unshift}."
+      puts "Currently serving #{array.shift}."
   end
 end
   
