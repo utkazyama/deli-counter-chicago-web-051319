59d7f91113d99dbcf3ef366de21a49a87a543a8d
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-22 18:35:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,11 @@
 def line(array)
-  list = []
   if array.length==0 
     puts "The line is currently empty."
   else
     current_line= "The line is currently:"
     array.each_with_index do |customer, index|
-      list.push("#{index+1}. #{customer}")
+      current_line << "#{index+1}. #{customer}"
     end
-    puts list
+    current_line
   end
 end
